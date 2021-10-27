548595ff2f96428a536474cdaf7a1af2211b6aca
princeantwi2k <antwi2k@gmail.com>
2021-10-27 13:09:15 +0800

    added Project

---
+++ src/Component/Page/Project.jsx
@@ -0,0 +1,246 @@
+import React from "react";
+import "./Project.css";
+import app1 from "../../Image/app1.png";
+import app2 from "../../Image/app2.png";
+import app3 from "../../Image/app3.png";
+import app4 from "../../Image/app4.png";
+import app5 from "../../Image/app5.png";
+import app6 from "../../Image/app6.png";
+import app7 from "../../Image/app7.png";
+
+const Project = () => {
+  return (
+    <>
+      <div class="project">
+        <div class="thumbnail">
+          <img src={app1} alt="app" className="app1" />
+          <div class="caption">
+            <h3 className="filler">BnB_Million</h3>
+            <p className="p">
+              {" "}
+              Do you want to stand out at that special occassion and be the
+              center of attention? If your answer is YES then look look no
+              further.bnb_millinery is here to make your dreams come true.We are
+              the cheapest on the market.We beat all prices.And the good news
+              is,we work with your budget.
+            </p>
+            <p>
+              <a
+                href="https://laughing-newton-2012ab.netlify.app"
+                class="btn btn-success buttonss"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/bnb_millenery"
+                class="btn btn-primary"
+                role="button"
+              >
+                View Code
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="thumbnail">
+          <img src={app2} alt="app1" className="app1" />
+          <div class="caption">
+            <h3 className="filler">Phyjo mines</h3>
+            <p className="p">
+              This is a mining website that just came in existing this year and
+              they are doing very good in their field of business right now in
+              the country , if not the best at the moment they are one of the
+              best in the country currently. Please visit their website if you
+              need their services and you can reach them on all the social media
+              handles. Thank You.
+            </p>
+            <p>
+              <a
+                href="https://confident-austin-7589d3.netlify.app/"
+                class="btn btn-success buttons"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/mines"
+                class="btn btn-primary"
+                role="button"
+              >
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="thumbnail">
+          <img src={app3} alt="app1" className="app1" />
+          <div class="caption">
+            <h3 className="filler">Think and Zoom</h3>
+            <p className="p">
+              This website help you to kwon of a goggle glass and Nuerosky EEG
+              headset ,you can zoom in on objects and see better by simply
+              thinking. that is the new technology in town so you have keep your
+              self abreast
+            </p>
+            <p>
+              <a
+                href="https://wizardly-goodall-3662a4.netlify.app/"
+                class="btn btn-success button "
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/think-and-zoom"
+                class="btn btn-primary"
+                role="button"
+              >
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="projects">
+        <div class="thumbnail">
+          <img src={app4} alt="app" className="app1" />
+          <div class="caption">
+            <h3 className="filler">Adom clinic</h3>
+            <p>
+              Adom clinic is a local based clinic that was build by a Software
+              developer by name Prince Antwi to help the local community get the
+              best healthcare that they deserve.
+            </p>
+            <p>
+              <a
+                href="https://adom-clinic-princeantwi2k.vercel.app/"
+                class="btn btn-success buttonss"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a href="#" class="btn btn-primary" role="button">
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="thumbnail">
+          <img src={app6} alt="app" className="app1" />
+          <div class="caption">
+            <h3 className="filler">Bluelet</h3>
+            <p>
+              Bluelet is an online shopping market that you yo can access to
+              your basic needs such as phones,laptops,printers and whole lot of
+              thing. just visit our website and be amaze.
+            </p>
+            <p>
+              <a
+                href="https://blog-five-iota.vercel.app/"
+                class="btn btn-success buttonss"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/blog"
+                class="btn btn-primary "
+                role="button"
+              >
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="thumbnail">
+          <img src={app5} alt="app" className="app1" />
+          <div class="caption">
+            <h3 className="filler">Filler Hub</h3>
+            <p>
+              Filler hub is a place you can get access to all the latest news is
+              the world being it politices,sport,business,science and magazine
+            </p>
+            <p>
+              <a
+                href="https://articles-git-master-princeantwi2k.vercel.app/"
+                class="btn btn-success button"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/articles"
+                class="btn btn-primary"
+                role="button"
+              >
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <div class="thumbnail">
+          <img src={app7} alt="app" className="app1" />
+          <div class="caption">
+            <h3 className="filler">My Movie Review</h3>
+            <p>
+              My Movie Review is a place you can get access to all the latest
+              movies is the world. feel free to visit this site if you are a
+              movie lover and you will really enjoy your stay
+            </p>
+            <p>
+              <a
+                href="https://articles-git-master-princeantwi2k.vercel.app/"
+                class="btn btn-success buttonsss"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/articles"
+                class="btn btn-primary"
+                role="button"
+              >
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="thumbnail">
+          <img src={app5} alt="app" className="app1" />
+          <div class="caption">
+            <h3 className="filler">Filler Hub</h3>
+            <p>
+              Filler hub is a place you can get access to all the latest news is
+              the world being it politices,sport,business,science and magazine
+            </p>
+            <p>
+              <a
+                href="https://articles-git-master-princeantwi2k.vercel.app/"
+                class="btn btn-success buttons"
+                role="button"
+              >
+                Demo
+              </a>{" "}
+              <a
+                href="https://github.com/Princeantwi2k/articles"
+                class="btn btn-primary"
+                role="button"
+              >
+                View code
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <h1>Portfolio Made By Prince Antwi</h1>
+        <p>Phone:+233 572 211 378</p>
+        <p>Follow me; Twitter @endtime24</p>
+      </div>
+    </>
+  );
+};
+
+export default Project;
