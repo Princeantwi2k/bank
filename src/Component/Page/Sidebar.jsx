2be9786ca934e88f33ae3a0e5d914d7fbca5cc2b
princeantwi2k <antwi2k@gmail.com>
2021-10-27 13:10:11 +0800

    added Sidebar

---
+++ src/Component/Page/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Sidebar = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <i class="bx bxs-home" />,
+    cName: "nav-text",
+  },
+  {
+    title: "About Me",
+    path: "/about",
+    icon: <i class="bx bx-user"></i>,
+    cName: "nav-text",
+  },
+  {
+    title: "Project",
+    path: "/project",
+    icon: <i class="bx bxl-product-hunt"></i>,
+    cName: "nav-text",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+    icon: <i class="bx bx-support"></i>,
+    cName: "nav-text",
+  },
+];
