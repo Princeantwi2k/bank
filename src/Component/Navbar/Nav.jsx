001d26295a519cf6ccd4811ed668b0156b8b49e5
princeantwi2k <antwi2k@gmail.com>
2021-10-27 13:07:15 +0800

    added Nav

---
+++ src/Component/Navbar/Nav.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+import { Sidebar } from "../Page/Sidebar";
+import { IconContext } from "react-icons";
+import img from "../../Image/me.jpg";
+
+const Nav = () => {
+  const [sidebar, setsidebar] = useState(false);
+  const showsidebar = () => setsidebar(!sidebar);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "red" }}>
+        <div className="navbar">
+          <img src={img} alt="profile" className="profile" />
+
+          <Link to="#" className="menu-bar">
+            <i className="bx bx-menu" onClick={showsidebar} />
+          </Link>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showsidebar}>
+              <li className="navbar-toggle">
+                <Link to="#" className="menu-bars">
+                  <i class="bx bx-window-close"></i>
+                </Link>
+              </li>
+              {Sidebar.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </IconContext.Provider>
+    </>
+  );
+};
+
+export default Nav;
