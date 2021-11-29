e3b65fb43a142efd6928a94675c031fd5521dd62
princeantwi2k <antwi2k@gmail.com>
2021-11-29 20:14:25 +0800

    added app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
     <Nav />
     <Switch >
       <Route path="/" exact component={Home} />
-    
+      <Route path="/project" component={Project} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
     </Switch>
     </BrowserRouter>
       
